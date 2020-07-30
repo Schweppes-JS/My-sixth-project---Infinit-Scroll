227c98361f834c274bacd389e7d80edff339bf60
Shvets Dmytro <shvetsdmytro90@ukr.net>
2020-07-30 12:15:12 +0300

    Third

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ const loader = document.getElementById('loader')
 
 let photosArray = [];
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
-        // Create <a> to link to Unsplush
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        })
         // Create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-        // Put <img> inside <a>, then put both inside imageContainer Element
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
